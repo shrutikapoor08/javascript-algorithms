4dd09435c119bbdbcfd11d0238d89eaeb83049fe
Shruti Kapoor <shruti.kapoor@telus.com>
2017-06-28 23:17:05 -0700

    adding algorithm for reversing string

---
+++ 27.js
@@ -0,0 +1,6 @@
+//Write a function reverseWords() that takes a string message and reverses the order of the words
+function reverseWords(s){
+  return s.split(' ').reverse().join(' ');
+}
+
+//TODO: reverse words in place
