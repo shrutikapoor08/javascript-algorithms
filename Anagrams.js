2b2e15f5c8c8f483d540c5ef4f3db7ff930776de
Shruti Kapoor <shruti.kapoor08@gmail.com>
2019-03-10 15:42:27 -0700

    anagrams

---
+++ Anagrams.js
@@ -0,0 +1,51 @@
+//Given two strings, write a function to determine if the second string is an anagram of the first string
+
+//create a counter for char in 1.
+//create a counter for char in 2.
+//loop through the counters to check if the key and frequency match.
+//return true if loop successfully completed, false otherwise
+
+function validAnagram(string1, string2) {
+
+
+    if(string1.length !== string2.length) {
+        return false;
+    }
+
+    const counter1 = {};
+    const counter2 = {};
+
+    for( let s of string1 ){
+        if(!counter1[s]) {
+            counter1[s] = 1;
+        }
+        else counter1[s] = counter1[s] +1;
+    }
+
+
+    for( let s of string2 ) {
+        if(!counter2[s]) {
+            counter2[s] = 1;
+        }
+        else {
+            counter2[s] = counter2[s] +1;
+        }
+    }
+
+    for( let key in counter1) {
+        if( !counter2[key] ||
+            (counter2[key] !== counter1[key])) {
+            return false;
+        }
+    }
+
+
+    return true;
+
+
+}
+
+console.log(validAnagram("", ""));
+console.log(validAnagram("23", "fhujghjfjg"));
+console.log(validAnagram("aaz", "aza"));
+console.log(validAnagram("aaz", "aaz"));
