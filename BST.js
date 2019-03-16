58f47732c15de3bcec1cb0d345d3f8642f0c2a7a
Shruti Kapoor <shrkapoor@paypal.com>
2019-03-16 12:47:34 -0700

    BST

--- BST.js
+++ BST.js
@@ -19,8 +19,61 @@ class BST {
     }
 
     insert(value) {
+        const node = new Node(value);
+        if(this.root === null) {
+            this.root = node;
+            return this;
+        }
 
+        let current = this.root;
+        while(true) {
+            if(value === current.value) return this;
+            if(value < current.value) {
+                if(current.left === null) {
+                    current.left = node;
+                    return this;
+                }
+                else current = current.left;
+            }
+            else if(value > current.value) {
+                if(current.right === null) {
+                    current.right = node;
+                    return this;
+                }
+                else current = current.right;
+            }
+        }
     }
+
+    contains(value) {
+        let current = this.root;
+        while(current) {
+            if (value === current.value) return true;
+            else if (value < current.value ) {
+                current = current.left;
+            }
+            else {
+                current = current.right;
+            }
+        }
+        return false;
+        }
 }
 
 var tree = new BST();
+tree.insert(10);
+tree.insert(8);
+tree.insert(5);
+tree.insert(9);
+tree.insert(12);
+tree.insert(11);
+tree.insert(18);
+tree.insert(10);
+console.log(JSON.stringify(tree));
+console.log(tree.contains(2));
+console.log(tree.contains(21));
+console.log(tree.contains(12));
+
+//          10
+//       8       12
+//    5    9  11   18
