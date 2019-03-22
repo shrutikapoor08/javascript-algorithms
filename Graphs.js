0706cf34153d7df1da30da122750d434aa0fa973
Shruti Kapoor <shrkapoor@paypal.com>
2019-03-21 17:41:12 -0700

    recursive dfs

--- Graphs.js
+++ Graphs.js
@@ -37,15 +37,69 @@ class Graph {
        delete list[vertex];
         console.log(list);
     }
+
+    depthFirstSearchRecursive(start) {
+        /*
+        if vertex is empty
+        return (base case)
+        add vertex to results list
+        mark vertex as visited
+        for each neighbor in vertex's neighbors:
+            if neighbor is not visited:
+            recursively call DFS
+
+        Helper function -
+            accepts a vertex
+            return early if vertex is empty
+            place the vertex in visited and push the vertex to result array
+            loop over all values in the adjacencyList for vertex
+            if any values not visited, recursively invoke the helper function
+         */
+        const visited = {};
+        const results = [];
+        const adjacencyList = this.adjacencyList;
+        DFSHelper(start);
+
+
+        function DFSHelper(vertex){
+            if(!vertex) return;
+            visited[vertex] = true;
+            results.push(vertex);
+           const adjacencies = adjacencyList[vertex];
+           adjacencies.forEach(adjacency => {
+               if(!visited[adjacency]) {
+                   DFSHelper(adjacency);
+               }
+           })
+        }
+        return results;
+    }
 }
 
 const g = new Graph();
-g.addVertex("Tokyo");
-g.addVertex("Vancouver");
-g.addVertex("San Francisco");
-g.addVertex("New Delhi");
-g.addEdge("Tokyo", "Vancouver");
-g.addEdge("Tokyo", "San Francisco");
-g.addEdge("New Delhi", "San Francisco");
-g.addEdge("New Delhi", "Tokyo");
-g.removeVertex("New Delhi");
+// g.addVertex("Tokyo");
+// g.addVertex("Vancouver");
+// g.addVertex("San Francisco");
+// g.addVertex("New Delhi");
+// g.addEdge("Tokyo", "Vancouver");
+// g.addEdge("Tokyo", "San Francisco");
+// g.addEdge("New Delhi", "San Francisco");
+// g.addEdge("New Delhi", "Tokyo");
+// g.removeVertex("New Delhi");
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("C", "A");
+g.addEdge("D", "B");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+// console.log(g.depthFirstSearchRecursive("A"));
